e1d60f2ebf20bd00fcda4a4d7722db82ed879b77
Sonia Mudhar <sonia.mudhar@ucalgary.ca>
2023-03-06 08:26:14 +0000

    adding another app.get

--- express_server.js
+++ express_server.js
@@ -11,8 +11,12 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => { //JSON string representing the entire urlDatabase object 
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
